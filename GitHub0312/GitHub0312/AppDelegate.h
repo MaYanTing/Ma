7da2822f8e29a1d0677996fcdc4dab7c3a740daf
MaYanTing <838502220@qq.com>
2016-03-12 14:51:10 -0800

    MYT
    add

---
+++ GitHub0312/GitHub0312/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitHub0312
+//
+//  Created by MaYanTing on 16/3/12.
+//  Copyright © 2016年 MAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitHub0312/GitHub0312/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitHub0312
+//
+//  Created by MaYanTing on 16/3/12.
+//  Copyright © 2016年 MAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
